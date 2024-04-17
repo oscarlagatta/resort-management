373fb0eed5a3639e2ec046bb06dff3e37b972b81
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-17 23:08:03 +0100

    Implement image handling in cabin creation
    Updated apiCabins.js to handle image file inputs during cabin creation. The image is now uploaded to the server and, in case of error, the newly created cabin entry is removed from the database. Made necessary adjustments in CreateCabinForm.jsx and FileInput.jsx to appropriately register the image input. Also exported supabaseUrl for use in apiCabins.js.

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -33,7 +33,8 @@ function CreateCabinForm() {
     console.log(errors)
 
     function onSubmit(data) {
-        mutate(data);
+        console.log(data)
+        mutate({...data, image: data.image[0]});
     }
 
     function onError(errors) {
@@ -105,7 +106,14 @@ function CreateCabinForm() {
         </FormRow>
 
         <FormRow label="Cabin photo">
-            <FileInput id="image" accept="image/*"/>
+            <FileInput
+                id="image"
+                accept="image/*"
+
+                {...register("image", {
+                    required: 'This field is required'
+                })}
+            />
         </FormRow>
 
         <FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
-import supabase from "./supabase";
+import supabase, {supabaseUrl} from "./supabase";
 
 export async function getCabins() {
-    const { data, cabins, error } = await supabase
+    const {data,  error} = await supabase
         .from('cabins')
         .select('*');
 
@@ -14,8 +14,9 @@ export async function getCabins() {
 
     return data;
 }
-export async function deleteCabin(id){
-    const { data,  error } = await supabase
+
+export async function deleteCabin(id) {
+    const {data, error} = await supabase
         .from('cabins')
         .delete()
         .eq("id", id);
@@ -30,13 +31,41 @@ export async function deleteCabin(id){
 }
 
 export async function createCabin(newCabin) {
-    const { data,  error } = await supabase
+
+    // If the cabin name contain any slashes then supabase will
+    // create folders based on that
+    const imageName = `${Math.random()}-${newCabin.image.name}`
+        .replaceAll("/", "");
+
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+    const {data, error} = await supabase
         .from('cabins')
-        .insert([newCabin])
+        .insert([{...newCabin, image: imagePath}]);
+
 
     if (error) {
         console.error(error);
         throw new Error(`Cabins could not be created`)
+    }
+
+    // Upload image
+    const {error: storageError} = await supabase
+        .storage
+        .from('cabin-images')
+        .upload(imageName, newCabin.image);
+
+
+    // Delete the cabin if was error uploading image
+    if (storageError) {
+
+        await supabase
+            .from('cabins')
+            .delete()
+            .eq("id", data.id);
+
+        throw new Error(`Cabins image could not be uploaded, 
+        the cabin was not created`);
     }
 
     return data;
