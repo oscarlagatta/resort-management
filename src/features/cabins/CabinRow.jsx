c7997f2044386557076f78dfe8f3c09e8e84328b
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-19 09:40:44 +0100

    Add duplication feature to CabinRow component
    Icons from "react-icons" package have been imported and used as buttons for cabin duplication, edit, and delete actions. A new react hook "useCreateCabin" has been imported to handle cabin duplication. This hook is used inside the newly created "handleDuplicate" function, which creates a new cabin entry using information from the current cabin as template.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,6 +3,8 @@ import {formatCurrency} from "../../utils/helpers.js";
 import {useState} from "react";
 import CreateCabinForm from "./CreateCabinForm.jsx";
 import {useDeleteCabin} from "./useDeleteCabin.js";
+import {HiPencil, HiSquare2Stack, HiTrash} from "react-icons/hi2";
+import {useCreateCabin} from "./useCreateCabin.js";
 
 const TableRow = styled.div`
     display: grid;
@@ -48,8 +50,21 @@ const CabinRow = ({cabin}) => {
     const [showForm, setShowForm] = useState(false);
     const { isDeleting, deleteCabin } = useDeleteCabin();
 
-    const {id: cabinId, name, maxCapacity, regularPrice, discount, image} = cabin;
+    const {id: cabinId, name, maxCapacity, regularPrice, discount, image, description} = cabin;
 
+    const  { isCreating, createCabin} = useCreateCabin();
+
+     function handleDuplicate() {
+        createCabin({
+            name: `Copy of ${name}`,
+            maxCapacity,
+            regularPrice,
+            discount,
+            image,
+            description,
+        });
+
+    }
 
     return (
         <>
@@ -60,8 +75,9 @@ const CabinRow = ({cabin}) => {
                 <Price>{formatCurrency(regularPrice)}</Price>
                 {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
                 <div>
-                    <button onClick={() => setShowForm(show => !show)}>Edit</button>
-                    <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>Delete</button>
+                    <button onClick={handleDuplicate}><HiSquare2Stack /></button>
+                    <button onClick={() => setShowForm(show => !show)}><HiPencil/></button>
+                    <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}><HiTrash /></button>
                 </div>
             </TableRow>
             {/*  Show/hide Form */}
