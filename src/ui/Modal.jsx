1916f4acecbd47d6d20690aeb43413059544b726
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-20 10:45:27 +0100

    The new hook, useOutsideClick, has been created and added to the project. This hook is now being utilized in the 'Modal' component to close the modal when a user clicks outside of it.

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {HiXMark} from "react-icons/hi2";
 import {createPortal} from "react-dom";
 import {cloneElement, createContext, useContext, useState} from "react";
+import {useOutsideClick} from "../hooks/useOutsideClick.js";
 
 const StyledModal = styled.div`
     position: fixed;
@@ -80,13 +81,16 @@ const Open = ({children, opens: opensWindowName}) => {
 
 const Window = ({children, name}) => {
 
-    const {openName, close, } = useContext(ModalContext);
+    const {openName, close,} = useContext(ModalContext);
+
+    const {ref} = useOutsideClick(close);
 
     if (name !== openName) return null;
 
+
     return createPortal(
         <Overlay>
-            <StyledModal>
+            <StyledModal ref={ref}>
                 <Button onClick={close}>
                     <HiXMark/>
                 </Button>
