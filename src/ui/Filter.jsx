58a4d9088515b14521b83f17ea97731cd7cb6bd7
Oscar Lagatta <oscar.lagatta@whistl.co.uk>
2024-04-22 16:42:21 +0100

    Add sorting functionality to cabin listing
    A new component SortBy has been added to provide users with options to sort cabin listing based on various parameters such as name, date, price, and capacity. This component utilizes the react-router-dom's useSearchParams to actively sort the cabin data. Changes have been made in CabinTable.jsx to filter and sort the cabins. The Select component has also been updated to accommodate the new sorting feature.

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
 import {useSearchParams} from "react-router-dom";
+import SortBy from "./SortBy.jsx";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -54,12 +55,23 @@ const Filter = ({filterField, options}) => {
                       key={option.value}
                       onClick={() => handleClick(option.value)}
                         active={option.value === currentFilter}
+                      disabled={option.value === currentFilter}
                   >
                       {option.label}
                   </FilterButton>
                   )
               )
           }
+          <SortBy options={[
+              {value: 'name-asc', label: 'Sort by name (A-Z)'},
+              {value: 'name-desc', label: 'Sort by name (Z-A)'},
+              {value: 'startDate-asc', label: 'Sort by Date ascending'},
+              {value: 'startDate-desc', label: 'Sort by Date descending'},
+              {value: 'regularPrice-asc', label: 'Sort by price (low first)'},
+              {value: 'regularPrice-desc', label: 'Sort by price (high first)'},
+              {value: 'maxCapacity-asc', label: 'Sort by capacity (low first)'},
+              {value: 'maxCapacity-desc', label: 'Sort by capacity (high first)'},
+          ]} />
 
       </StyledFilter>
   )
---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,22 @@
+import Select from "./Select.jsx";
+import {useSearchParams} from "react-router-dom";
+
+export default function SortBy({options}) {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const sortBy = searchParams.get('sortBy') || '';
+
+    function handleChange(e) {
+        searchParams.set('sortBy', e.target.value);
+        setSearchParams(searchParams);
+    }
+
+    return (
+        <Select
+            options={options}
+            type='white'
+            value={sortBy}
+            onChange={handleChange} />
+    )
+}
