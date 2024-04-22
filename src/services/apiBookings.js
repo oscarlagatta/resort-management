3988573c435a3f04b15ad36c1fd4b8eb784e1819
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-22 20:56:37 +0100

    Add sorting functionality to bookings API
    Expanded the functionality of the getBookings API endpoint and the useBookings hook to include a sorting option. This allows for more specific queries when fetching bookings from the database. Also, refined the filter check to ensure non-null values only.

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -13,10 +13,14 @@ export async function getBookings({ filter, sortBy}) {
             "cabins(name), guests(fullName,email)");
 
     // FILTER
-    if (filter !== null) {
+    if (filter) {
         query = query[filter.method || 'eq'](filter.field, filter.value)
     }
 
+    // SORT
+    if(sortBy) query = query.order(sortBy.field, {
+        ascending: sortBy.direction === 'asc'
+    });
 
     const { data, error } = await query;
 
