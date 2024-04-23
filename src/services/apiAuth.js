187718aa1f912edc832024940524d56ef57ba587
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-23 23:42:15 +0100

    User login with Supabase
    Implemented a more complex login procedure by integrating new UI components and services. The login form now fetches from a new API authentication function. Additionally, introduced error handlings, loading state, and successful login redirect logic. Created reusable UI components such as Logo, Heading, and FormRowVertical along the way.

---
+++ src/services/apiAuth.js
@@ -0,0 +1,16 @@
+import supabase from "./supabase.js";
+
+export async function login({email, password}) {
+
+    let {data, error} = await supabase
+        .auth
+        .signInWithPassword({
+            email,
+            password,
+        })
+
+
+    if (error ) throw new Error(error.message);
+
+    return data;
+}
