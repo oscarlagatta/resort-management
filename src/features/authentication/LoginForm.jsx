291799eaedfb6061e1e79a6528e0451b1d62f839
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-25 22:28:02 +0100

    Remove hardcoded credentials from LoginForm
    The hardcoded email and password states in LoginForm have been removed. This will ensure the login fields start empty and promote better security practices.

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import {useLogin} from "./useLogin.js";
 import SpinnerMini from "../../ui/SpinnerMini.jsx";
 
 function LoginForm() {
-    const [email, setEmail] = useState("oscarlagatta@gmail.com");
-    const [password, setPassword] = useState("PlanetOL233");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const {login, isLoading} = useLogin();
 
