1dad3779fae5e4e73073db134980058a88616d31
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-04-25 22:05:30 +0100

    Add error boundary for better error handling
    The `react-error-boundary` dependency has been added. A new component `ErrorFallback.jsx` is created which will display when an error occurs. This increases app resilience and improves user experience by providing a dedicated error message and recovery option.

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading.jsx";
+import GlobalStyles from "../styles/GlobalStyles.js";
+import Button from "./Button.jsx";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,19 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+export default function ErrorFallback({error, resetErrorBoundary}) {
+    return (
+        <>
+            <GlobalStyles/>
+            <StyledErrorFallback>
+                <Box>
+                    <Heading as='h1'>Something went wrong !</Heading>
+                    <p>{error.message}</p>
+                    <Button size='large' onClick={resetErrorBoundary}>Try again</Button>
+                </Box>
+            </StyledErrorFallback>
+        </>
+
+    )
+}
